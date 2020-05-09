7e3c2c6b7380f5ab7b8f901c64606112a60aca49
Lauren Doss <doss.laurene@gmail.com>
2020-05-08 17:23:34 -0700

    transferred to new repository

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+$("#buttonAboutMe").click(function(){
+    window.location.href="aboutMe.html"
+})
+$("#buttonContact").click(function(){
+    window.location.href="contact.html"
+})
+$("#buttonPortfolio").click(function(){
+    window.location.href="portfolio.html"
+})
+
+$('.ui.sticky')
+  .sticky({
+    context: '#context'
+  })
+;
